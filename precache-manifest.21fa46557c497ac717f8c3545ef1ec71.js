9ee6aec3a7d3802944ce373ef15885a30ac90a74
Jonas Kelstrup <jonaskelstrup@gmail.com>
2019-01-30 18:34:51 +0100

    Updates

---
+++ precache-manifest.21fa46557c497ac717f8c3545ef1ec71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b56eed4afca58f7f556",
+    "url": "/jonas-rasmus/static/js/runtime~main.4b56eed4.js"
+  },
+  {
+    "revision": "1cb031cd3e2263ef19f0",
+    "url": "/jonas-rasmus/static/js/main.1cb031cd.chunk.js"
+  },
+  {
+    "revision": "4be8c448ee6b10c3efab",
+    "url": "/jonas-rasmus/static/js/1.4be8c448.chunk.js"
+  },
+  {
+    "revision": "1cb031cd3e2263ef19f0",
+    "url": "/jonas-rasmus/static/css/main.c8e5ffea.chunk.css"
+  },
+  {
+    "revision": "a4dce70d89a56fcf2f29f38d43d54c97",
+    "url": "/jonas-rasmus/index.html"
+  }
+];
